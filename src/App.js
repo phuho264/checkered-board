ccf772b5c181591e8403252f9de44696130923da
Phu Ho <fooho264@gmail.com>
2020-04-15 15:21:15 -0700

    uncomment

--- src/App.js
+++ src/App.js
@@ -1,53 +1,3 @@
-/*import React , {Component} from 'react';
-import './App.css';
-
-class Board extends Component{
-  render(){
-    var color = 0;
-    var squares = []
-    for(let i = 0; i < 64; i++){
-      if(i%16===0){
-        squares.push(<div className="square-yellow">{i}</div>);
-        color=0;
-      }
-      else if (i%8 === 0){
-        squares.push(<div className="square-orange">{i}</div>);
-        color =1;
-      }
-      else if (color ===1){
-        squares.push(<div className="square-yellow">{i}</div>);
-        color=0;
-      }
-      else{
-        squares.push(<div className="square-orange">{i}</div>);
-        color =1;
-      }
-    }
-    return(
-      <div className="board">{squares}</div> 
-    );
-  }
-}
-
-class App extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state={
-  //     newNumber: 0
-  //   }
-  // }
-
-  render(){
-    
-    return (
-      <div className="App">
-      <Board/>
-      </div>
-    );  
-  }
-  
-}
-export default App; */
 import React , {Component} from 'react';
 import './App.css';
 
@@ -97,5 +47,4 @@ class App extends Component {
   }
   
 }
-export default App;
-
+export default App; 
